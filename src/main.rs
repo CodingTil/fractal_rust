50f243640066c336fb28102d1de18e232db56606
CodingTil <36734749+CodingTil@users.noreply.github.com>
2023-03-07 16:55:41 +0100

    Added Command Line Argument Parser

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,36 @@ use rand;
 use rayon::prelude::*;
 use std::time::SystemTime;
 use noise::{NoiseFn, Perlin};
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+   /// Anti aliasing factor
+   #[arg(short, long, default_value_t = 4)]
+   anti_aliasing: usize,
+
+   /// Maximum iterations
+   #[arg(short, long, default_value_t = 5000)]
+   max_iterations: usize,
+
+   /// Width of the image
+   #[arg(short, long, default_value_t = 1920)]
+   width: usize,
+
+   /// Height of the image
+   #[arg(short, long, default_value_t = 1080)]
+   height: usize,
+}
 
 fn main() {
-	const MAX_ITERATIONS: usize = 5000;
-	const ANTI_ALIASING: usize = 4;
+	let args: Args = Args::parse();
+
 	let now = SystemTime::now();
 	let perlin_re = Perlin::new(0);
 	let perlin_im = Perlin::new(0);
 
-	let canvas = Canvas::new(1920, 1080)
+	let canvas = Canvas::new(args.width, args.height)
 		.title("Fractal")
 		.hidpi(true)
 		.show_ms(true);
@@ -25,18 +46,18 @@ fn main() {
 			im: perlin_im.get([0.0, current_time, 0.0]),
 		};
 
-		let height = image.height() as f64;
-		let width = image.width() as f64;
+		let height = args.height as f64;
+		let width = args.width as f64;
 		let height_scale: f64 = 1.0 / (height / 2.0);
 		let width_scale: f64 = 1.0 / (width / 2.0);
 		let scale = height_scale.max(width_scale);
 
 		let pixel_iterations = image
-			.par_chunks(width as usize)
+			.par_chunks(args.width)
 			.enumerate()
 			.map(|(y, row)| {
 				row.par_iter().enumerate().map(move |(x, _)| {
-					let iterations = (0..ANTI_ALIASING).into_par_iter().map(|_| {
+					let iterations = (0..args.anti_aliasing).into_par_iter().map(|_| {
 						let x_offset = rand::random::<f64>() - 0.5;
 						let y_offset = rand::random::<f64>() - 0.5;
 						fractal::compute_iterations_smooth(
@@ -45,9 +66,9 @@ fn main() {
 								im: ((y as f64 + y_offset) - height / 2.0) * scale,
 							},
 							constant,
-							MAX_ITERATIONS,
+							args.max_iterations,
 						)
-					}).sum::<usize>() as f64 / ANTI_ALIASING as f64;
+					}).sum::<usize>() as f64 / args.anti_aliasing as f64;
 					iterations
 				}).collect::<Vec<_>>()
 			}).collect::<Vec<_>>();
