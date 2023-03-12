049d9152c5053fa447c6d6c83189fc93aac34269
CodingTil <36734749+CodingTil@users.noreply.github.com>
2023-03-12 16:15:38 +0100

    Added support to draw to existing DOM Element

--- src/lib.rs
+++ src/lib.rs
@@ -7,9 +7,6 @@ use winit::{
 	window::{Window, WindowBuilder},
 };
 
-#[cfg(target_arch = "wasm32")]
-use wasm_bindgen::prelude::*;
-
 #[repr(C)]
 #[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
 struct DimensionUniform {
@@ -431,7 +428,6 @@ impl State {
 	}
 }
 
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen(start))]
 pub async fn run() {
 	cfg_if::cfg_if! {
 		if #[cfg(target_arch = "wasm32")] {
@@ -461,6 +457,90 @@ pub async fn run() {
 				dst.append_child(&canvas).ok()?;
 				Some(())
 			})
+			.expect("Couldn't append canvas to document body.");
+	}
+
+	// State::new uses async code, so we're going to wait for it to finish
+	let mut state = State::new(window).await;
+
+	event_loop.run(move |event, _, control_flow| {
+		match event {
+			Event::WindowEvent {
+				ref event,
+				window_id,
+			} if window_id == state.window().id() => {
+				if !state.input(event) {
+					match event {
+						WindowEvent::CloseRequested
+						| WindowEvent::KeyboardInput {
+							input:
+								KeyboardInput {
+									state: ElementState::Pressed,
+									virtual_keycode: Some(VirtualKeyCode::Escape),
+									..
+								},
+							..
+						} => *control_flow = ControlFlow::Exit,
+						WindowEvent::Resized(physical_size) => {
+							state.resize(*physical_size);
+						}
+						WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+							// new_inner_size is &mut so w have to dereference it twice
+							state.resize(**new_inner_size);
+						}
+						_ => {}
+					}
+				}
+			}
+			Event::RedrawRequested(window_id) if window_id == state.window().id() => {
+				state.update();
+				match state.render() {
+					Ok(_) => {}
+					// Reconfigure the surface if it's lost or outdated
+					Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+						state.resize(state.size)
+					}
+					// The system is out of memory, we should probably quit
+					Err(wgpu::SurfaceError::OutOfMemory) => *control_flow = ControlFlow::Exit,
+					// We're ignoring timeouts
+					Err(wgpu::SurfaceError::Timeout) => log::warn!("Surface timeout"),
+				}
+			}
+			Event::MainEventsCleared => {
+				// RedrawRequested will only trigger once, unless we manually
+				// request it.
+				state.window().request_redraw();
+			}
+			_ => {}
+		}
+	});
+}
+
+#[cfg(target_arch = "wasm32")]
+pub async fn run_with_html_element(element: &web_sys::Element) {
+	cfg_if::cfg_if! {
+		if #[cfg(target_arch = "wasm32")] {
+			std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+			console_log::init_with_level(log::Level::Warn).expect("Could't initialize logger");
+		} else {
+			env_logger::init();
+		}
+	}
+
+	let event_loop = EventLoop::new();
+	let window = WindowBuilder::new().build(&event_loop).unwrap();
+
+	#[cfg(target_arch = "wasm32")]
+	{
+		// Winit prevents sizing with CSS, so we have to set
+		// the size manually when on web.
+		use winit::dpi::PhysicalSize;
+		window.set_inner_size(PhysicalSize::new(1000, 600));
+
+		use winit::platform::web::WindowExtWebSys;
+		let canvas = web_sys::Element::from(window.canvas());
+		element
+			.append_child(&canvas)
 			.expect("Couldn't append canvas to document body.");
 	}
 
